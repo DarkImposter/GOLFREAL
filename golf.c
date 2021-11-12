8ce7544329c1a56873172c611e3b634083fa56c1
DarkImposter <33733120+DarkImposter@users.noreply.github.com>
2021-11-11 22:19:25 -0500

    Initial commit

---
+++ golf.c
@@ -0,0 +1,21 @@
+// ECE 209 - Fall 2021 - Program 3
+// This program reads information about a golf tournament
+// and allows a user to view information about the course,
+// the overall scores, and the individual players.
+//
+// Implementation of Course ADT and functions for Player and PlayerNode types
+//
+// Parker Sexton
+
+#include "golf.h"
+#include <stdio.h>
+
+
+
+
+Course readCourse(const char * fn){
+
+}
+
+
+
